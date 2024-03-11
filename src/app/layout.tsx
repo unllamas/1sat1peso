62680b850d3c17f5b6a68ded6b31231095a0a7ce
Jona <llamasjonat@gmail.com>
2024-03-11 20:19:16 -0300

    fix: new og metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,18 @@
-import type { Metadata } from "next";
-import Script from "next/script";
+import type { Metadata } from 'next';
+import Script from 'next/script';
 
-import StyledComponentsRegistry from "@/utils/registry";
-import { GOOGLE_TAG_ID } from "@/constants/config";
-import { fontPrimary, fontSecondary } from "@/styles/fonts";
+import StyledComponentsRegistry from '@/utils/registry';
+import { GOOGLE_TAG_ID } from '@/constants/config';
+import { fontPrimary, fontSecondary } from '@/styles/fonts';
 
 export const metadata: Metadata = {
-  title: "1 Satoshi 1 Peso | La Crypta",
+  title: '1 Satoshi 1 Peso | La Crypta',
   description:
-    "La hiperbitcoinización es inminente, por eso enterate cuánto falta para el famoso 1 a 1 del Peso Argentino contra Bitcoin.",
+    'La hiperbitcoinización es inminente, por eso enterate cuánto falta para el famoso 1 a 1 del Peso Argentino contra Bitcoin.',
+
+  openGraph: {
+    images: '/api/og',
+  },
 };
 
 export default function RootLayout({
@@ -17,44 +21,37 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="es"
-      className={`${fontPrimary.variable} ${fontSecondary.variable}`}
-    >
+    <html lang='es' className={`${fontPrimary.variable} ${fontSecondary.variable}`}>
       <head>
-        <meta name="viewport" content="width=device-width, user-scalable=no" />
-        <link rel="icon" type="img/png" href="img/schema-logo.png" />
+        <meta name='viewport' content='width=device-width, user-scalable=no' />
+        <link rel='icon' type='img/png' href='img/schema-logo.png' />
 
         {/*  */}
-        <meta name="author" content="La Crypta" />
-        <meta name="robots" content="index,follow" />
+        <meta name='author' content='La Crypta' />
+        <meta name='robots' content='index,follow' />
 
         {/* Facebook */}
-        <meta property="og:title" content="1 Satoshi 1 Peso | La Crypta" />
+        <meta property='og:title' content='1 Satoshi 1 Peso | La Crypta' />
         <meta
-          property="og:description"
-          content="La hiperbitcoinización es inminente, por eso enterate cuánto falta para el famoso 1 a 1 del Peso Argentino contra Bitcoin."
+          property='og:description'
+          content='La hiperbitcoinización es inminente, por eso enterate cuánto falta para el famoso 1 a 1 del Peso Argentino contra Bitcoin.'
         />
-        <meta property="og:locale" content="es_ES" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="/img/social/facebook-1200x630.jpg" />
-        <meta property="og:url" content="https://1satoshi1peso.ar/" />
+        <meta property='og:locale' content='es_ES' />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://1satoshi1peso.ar/' />
 
         {/* Twitter */}
-        <meta name="twitter:title" content="1 Satoshi 1 Peso | La Crypta" />
+        <meta name='twitter:title' content='1 Satoshi 1 Peso | La Crypta' />
         <meta
-          name="twitter:description"
-          content="La hiperbitcoinización es inminente, por eso enterate cuánto falta para el famoso 1 a 1 del Peso Argentino contra Bitcoin."
+          name='twitter:description'
+          content='La hiperbitcoinización es inminente, por eso enterate cuánto falta para el famoso 1 a 1 del Peso Argentino contra Bitcoin.'
         />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="/img/social/twitter-600x330.jpg" />
-        <meta name="twitter:url" content="https://1satoshi1peso.ar/" />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:url' content='https://1satoshi1peso.ar/' />
 
         {/* Analytics */}
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}`}
-        />
-        <Script id="google-analytics">
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}`} />
+        <Script id='google-analytics'>
           {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
